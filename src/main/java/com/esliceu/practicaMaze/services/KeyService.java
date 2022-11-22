8d7613337fcf7276cc1f173e2c5ffedf4146773d
Jalopezm <jalopezm@esliceu.net>
2022-11-22 18:14:05 +0100

    HAER FILTROS ANIMACION NORTE E HECHA

--- src/main/java/com/esliceu/practicaMaze/services/KeyService.java
+++ src/main/java/com/esliceu/practicaMaze/services/KeyService.java
@@ -18,12 +18,27 @@ public class KeyService {
             if (totalCoin >= key.getValue()){
                 player.addItem(key);
                 room.deleteItem(0);
+                deleteCoin(totalCoin,inv);
                 break;
-            }else{
-                inv.remove(i);
             }
         }
         System.out.println(totalCoin);
         System.out.println(key.getValue());
+    }
+
+    private void deleteCoin(int totalCoin, List<Item> inv) {
+        int counter=0;
+        for (int i = 0; i < inv.size(); i++) {
+            String item = inv.get(i).toString();
+            System.out.println(item);
+            if (item.equals("Coin")){
+                counter++;
+                inv.remove(i);
+            }
+            if (counter == totalCoin){
+                counter=0;
+                break;
+            }
+        }
     }
 }
--- src/main/webapp/js/javaScript.js
+++ src/main/webapp/js/javaScript.js
@@ -2,9 +2,12 @@ canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 let dataScript = document.getElementById("myjson").textContent;
 let data = JSON.parse(dataScript);
+let pj;
+let start, previousTimeStamp;
+let done = false
 
 const srcs = ["img/backGround.png", "img/coin.png", "img/key.png", "img/wallH.png", "img/wallV.png",
-    "img/doorH.png", "img/doorV.png"];
+    "img/doorH.png", "img/doorV.png","img/pj.png"];
 const images = srcs.map((src) => {
     const image = new Image();
     image.src = src;
@@ -19,7 +22,6 @@ images.forEach((image) => {
     };
 });
 
-
 canvas.addEventListener('click', event => {
     let rect = canvas.getBoundingClientRect();
     let x = Math.round(event.clientX - rect.left);
@@ -30,17 +32,20 @@ canvas.addEventListener('click', event => {
         window.location.assign("/open?dir=North");
     }
     if (x >= 785 & y >= 371 && x <= 915 && y <= 387) {
+         requestAnimationFrame(moveS);
         window.location.assign("/open?dir=South");
     }
     if (x >= 640 && y >= 190 && x <= 652 && y <= 319) {
+         requestAnimationFrame(moveW);
         window.location.assign("/open?dir=West");
     }
     if (x >= 1018 && y >= 190 && x <= 1033 && y <= 319) {
+         requestAnimationFrame(moveE);
         window.location.assign("/open?dir=East");
     }
     if (x >= 702 && y >= 9 && x <= 974 && y <= 56) {
         console.log("N")
-        window.location.assign("/nav?dir=North");
+        requestAnimationFrame(moveN);
     }
     if (x >= 702 & y >= 431 && x <= 974 && y <= 477) {
         console.log("S")
@@ -63,7 +68,9 @@ canvas.addEventListener('click', event => {
         window.location.assign("/getKey");
     }
 });
+
 function draw(images) {
+    pj=images[7];
     ctx.drawImage(images[0], 0, 0);
     if (data.items.Coin != "[null]") {
         ctx.drawImage(images[1], 945, 298, 60, 60);
@@ -112,3 +119,37 @@ function draw(images) {
             break;
     }
 };
+let numero = 0;
+
+function moveN(timestamp) {
+
+    if (start === undefined) {
+        start = timestamp;
+    }
+    const elapsed = timestamp - start;
+
+    if (previousTimeStamp !== timestamp) {
+        const count = Math.floor(0.1 * elapsed, 100);
+        draw(images);
+        console.log(count)
+        console.log(numero)
+        ctx.drawImage(pj, numero * 39, 3 * 62, 39, 62,
+            810, 200 - count, 39 * 1.2, 62 * 1.2);
+            if(count%20 == 0){
+                        numero+=1;
+                         if (numero > 3){
+                                                numero = 0;
+                        }
+                    }
+        if (count === 100) done = true;
+    }
+
+    if (elapsed < 5000) {
+        previousTimeStamp = timestamp;
+        if (!done) {
+            window.requestAnimationFrame(moveN);
+        }else{
+            window.location.assign("/nav?dir=North");
+        }
+    }
+}
